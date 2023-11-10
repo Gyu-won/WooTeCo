7f670c05303d00925db0445d41804e0aa4d2e8de
JUNG GYUWON <milestogo0905@naver.com>
2023-11-10 10:41:54 +0900

    feat: 방문날짜가 12월 1일에서 25일 사이인지 확인하는 기능 추가
    크리스마스 디데이 할인 이벤트 기간인 12월 1일에서 25일 사이에 예약날짜가 포함되는지 확인하는 기능 추가.

--- src/main/java/christmas/domain/ChristmasDiscount.java
+++ src/main/java/christmas/domain/ChristmasDiscount.java
@@ -1,7 +1,9 @@
 package christmas.domain;
 
 public class ChristmasDiscount {
+    private static final Integer EVENT_END_DATE = 25;
+
     public static boolean isWithinPeriod(Integer reserveDate) {
-        return true;
+        return reserveDate <= EVENT_END_DATE;
     }
 }
