c809889715d510b00e5c917cf8df496b7ccfd16a
JUNG GYUWON <milestogo0905@naver.com>
2023-11-10 10:34:58 +0900

    test: 크리스마스 할인 이벤트 적용 가능 여부 확인 테스트 추가

---
+++ src/main/java/christmas/domain/ChristmasDiscount.java
@@ -0,0 +1,7 @@
+package christmas.domain;
+
+public class ChristmasDiscount {
+    public static boolean isWithinPeriod(Integer reserveDate) {
+        return true;
+    }
+}
---
+++ src/test/java/christmas/domain/ChristmasDiscountTest.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ChristmasDiscountTest {
+
+    @DisplayName("크리스마스 디데이 할인 날짜에 포함되는 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {23, 1, 25})
+    public void 크리스마스_할인_기간에_포함되는_경우(Integer reserveDate) {
+        //when
+        boolean isWithinPeriod = ChristmasDiscount.isWithinPeriod(reserveDate);
+
+        //then
+        assertThat(isWithinPeriod).isTrue();
+    }
+}
