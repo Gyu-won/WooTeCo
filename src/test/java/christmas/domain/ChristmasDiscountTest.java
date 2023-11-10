e20cfcc6b1495edf2fd2adec2165ab880f3067f3
JUNG GYUWON <milestogo0905@naver.com>
2023-11-10 10:37:14 +0900

    test: 크리스마스 할인 이벤트 기간에 포함되지 않는 경우 테스트 추가

--- src/test/java/christmas/domain/ChristmasDiscountTest.java
+++ src/test/java/christmas/domain/ChristmasDiscountTest.java
@@ -18,4 +18,15 @@ public class ChristmasDiscountTest {
         //then
         assertThat(isWithinPeriod).isTrue();
     }
+
+    @DisplayName("크리스마스 디데이 할인 날짜에 포함되지 않는 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {26, 31})
+    public void 크리스마스_할인_기간에_포함되지_않는_경우(Integer reserveDate) {
+        //when
+        boolean isWithinPeriod = ChristmasDiscount.isWithinPeriod(reserveDate);
+
+        //then
+        assertThat(isWithinPeriod).isFalse();
+    }
 }
