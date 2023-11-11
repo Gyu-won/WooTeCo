c5c91026767356e21a72c3203e6cdb532de604ce
JUNG GYUWON <milestogo0905@naver.com>
2023-11-11 21:04:19 +0900

    test: 총 주문금액이 10000원 미만인 경우 이벤트를 적용하지 않는 테스트 추가

--- src/test/java/christmas/domain/entity/OrderDetailsTest.java
+++ src/test/java/christmas/domain/entity/OrderDetailsTest.java
@@ -4,21 +4,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OrderDetailsTest {
 
+    private Map<Menu, Integer> orderMenus;
+
+    @BeforeEach
+    public void setUp() {
+        orderMenus = new HashMap<>();
+    }
+
     @DisplayName("총 이벤트 할인 금액을 계산한다.")
     @Test
     public void 모든_이벤트_적용_후_할인금액을_계산() {
         //given
-        Map<Menu, Integer> orderMenus = new HashMap<>();
         orderMenus.put(Menu.TBoneSteak, 1);
         orderMenus.put(Menu.BarbecueRibs, 1);
         orderMenus.put(Menu.ChocolateCake, 2);
         orderMenus.put(Menu.ZeroCoke, 1);
-
         OrderDetails orderDetails = new OrderDetails(3, 142000, orderMenus);
 
         //when
@@ -26,5 +32,20 @@ public class OrderDetailsTest {
 
         //then
         assertThat(eventReward.sumEventRewards()).isEqualTo(31246);
+    }
+
+    @DisplayName("총 주문 금액이 10000원 미만인 경우에는 이벤트를 적용하지 않는다.")
+    @Test
+    public void 모든_이벤트를_적용하지_않는_경우() {
+        //given
+        orderMenus.put(Menu.Tapas, 1);
+        orderMenus.put(Menu.ZeroCoke, 1);
+        OrderDetails orderDetails = new OrderDetails(25, 8500, orderMenus);
+
+        //when
+        EventReward eventReward = orderDetails.calculateEventReward();
+
+        //then
+        assertThat(eventReward.sumEventRewards()).isEqualTo(0);
     }
 }
