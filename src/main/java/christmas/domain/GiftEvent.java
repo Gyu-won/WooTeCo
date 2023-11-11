83ec09a010ade9313d94b944c7b4b7bce7610542
JUNG GYUWON <milestogo0905@naver.com>
2023-11-11 14:23:16 +0900

    feat: 증정 이벤트 할인 적용 가능 여부를 판단하는 기능 추가

--- src/main/java/christmas/domain/GiftEvent.java
+++ src/main/java/christmas/domain/GiftEvent.java
@@ -1,8 +1,9 @@
 package christmas.domain;
 
 public class GiftEvent {
+    private static final Integer MINIMUM_ORDER_AMOUNT = 120000;
 
-    public static boolean isWithinPeriod(Integer totalPurchaseAmount) {
-        return true;
+    public static boolean isWithinPeriod(Integer totalOrderAmount) {
+        return totalOrderAmount >= MINIMUM_ORDER_AMOUNT;
     }
 }
--- src/test/java/christmas/domain/GiftEventTest.java
+++ src/test/java/christmas/domain/GiftEventTest.java
@@ -11,9 +11,9 @@ public class GiftEventTest {
     @DisplayName("증정 이벤트에 해당하면 증정이벤트를 적용한다..")
     @ParameterizedTest
     @ValueSource(ints = {120000, 150000})
-    public void 총_주문금액이_12만원_이상인_경우(Integer totalPurchaseAmount) {
+    public void 총_주문금액이_12만원_이상인_경우(Integer totalOrderAmount) {
         //when
-        boolean isWithinPeriod = GiftEvent.isWithinPeriod(totalPurchaseAmount);
+        boolean isWithinPeriod = GiftEvent.isWithinPeriod(totalOrderAmount);
 
         //then
         assertThat(isWithinPeriod).isTrue();
@@ -22,9 +22,9 @@ public class GiftEventTest {
     @DisplayName("증정 이벤트에 해당하지 않으면 증정이벤트를 적용하지 않는다.")
     @ParameterizedTest
     @ValueSource(ints = {10000, 119999})
-    public void 총_주문금액이_12만원_이하인_경우(Integer totalPurchaseAmount) {
+    public void 총_주문금액이_12만원_이하인_경우(Integer totalOrderAmount) {
         //when
-        boolean isWithinPeriod = GiftEvent.isWithinPeriod(totalPurchaseAmount);
+        boolean isWithinPeriod = GiftEvent.isWithinPeriod(totalOrderAmount);
 
         //then
         assertThat(isWithinPeriod).isFalse();
