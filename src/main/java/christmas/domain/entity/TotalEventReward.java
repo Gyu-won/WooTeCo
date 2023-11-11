635b6893e0f94a356cb0436c6c2f866f718cbf17
JUNG GYUWON <milestogo0905@naver.com>
2023-11-11 19:37:18 +0900

    test: 총 할인 금액의 합을 계산하는 테스트 추가

--- src/main/java/christmas/domain/entity/TotalEventReward.java
+++ src/main/java/christmas/domain/entity/TotalEventReward.java
@@ -1,4 +1,27 @@
 package christmas.domain.entity;
 
 public class TotalEventReward {
+    private final Integer christmasDiscount;
+    private final Integer weekdayDiscount;
+    private final Integer holidayDiscount;
+    private final Integer specialDiscount;
+    private final Integer giftEvent;
+
+    private TotalEventReward(Integer christmasDiscount, Integer weekdayDiscount, Integer holidayDiscount,
+                             Integer specialDiscount, Integer giftEvent) {
+        this.christmasDiscount = christmasDiscount;
+        this.weekdayDiscount = weekdayDiscount;
+        this.holidayDiscount = holidayDiscount;
+        this.specialDiscount = specialDiscount;
+        this.giftEvent = giftEvent;
+    }
+
+    public static TotalEventReward create(Integer christmasDiscount, Integer weekdayDiscount, Integer holidayDiscount,
+                                          Integer specialDiscount, Integer giftEvent) {
+        return new TotalEventReward(christmasDiscount, weekdayDiscount, holidayDiscount, specialDiscount, giftEvent);
+    }
+
+    public Integer sumEventDiscounts() {
+        return null;
+    }
 }
--- src/test/java/christmas/domain/entity/TotalEventRewardTest.java
+++ src/test/java/christmas/domain/entity/TotalEventRewardTest.java
@@ -1,4 +1,29 @@
 package christmas.domain.entity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class TotalEventRewardTest {
+
+    @DisplayName("총 할인 금액의 합을 계산한다.")
+    @Test
+    public void 총_할인금액을_계산() {
+        //given
+        Integer christmasDiscount = 1200;
+        Integer weekdayDiscount = 4046;
+        Integer holidayDiscount = 0;
+        Integer specialDiscount = 1000;
+        Integer giftEvent = 25000;
+
+        TotalEventReward totalEventReward = TotalEventReward.create(christmasDiscount, weekdayDiscount, holidayDiscount,
+                specialDiscount, giftEvent);
+
+        //when
+        Integer totalEventDiscounts = totalEventReward.sumEventDiscounts();
+
+        //then
+        assertThat(totalEventDiscounts).isEqualTo(31246);
+    }
 }
