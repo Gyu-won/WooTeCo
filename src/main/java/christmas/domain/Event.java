515f0bef12c750f08fb462fb1813cb6225129731
JUNG GYUWON <milestogo0905@naver.com>
2023-11-11 15:17:43 +0900

    feat: 총 할인 금액의 합을 계산하는 기능 추가

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -3,16 +3,48 @@ package christmas.domain;
 import java.util.Map;
 
 public class Event {
+    private static final Integer NO_DISCOUNT = 0;
 
     public static Integer applyGiftEvent(Integer totalOrderAmount) {
         if (GiftEvent.isApplicable(totalOrderAmount)) {
             return Menu.calculateGiftPrice();
         }
-        return 0;
+        return NO_DISCOUNT;
     }
 
     public static Integer applyDiscountEvent(Map<Menu, Integer> orderItems, Integer reserveDate) {
-        int totalDiscount = 0;
+        Integer totalDiscount = applyChristmasDiscount(reserveDate);
+        totalDiscount += applyWeekdayDiscount(orderItems, reserveDate);
+        totalDiscount += applyHolidayDiscount(orderItems, reserveDate);
+        totalDiscount += applySpecialDiscount(reserveDate);
         return totalDiscount;
+    }
+
+    private static Integer applyChristmasDiscount(Integer reserveDate) {
+        if (ChristmasDiscount.isWithinPeriod(reserveDate)) {
+            return ChristmasDiscount.calculate(reserveDate);
+        }
+        return NO_DISCOUNT;
+    }
+
+    private static Integer applyWeekdayDiscount(Map<Menu, Integer> orderItems, Integer reserveDate) {
+        if (WeekdayDiscount.isWithinPeriod(reserveDate)) {
+            return WeekdayDiscount.calculate(orderItems);
+        }
+        return NO_DISCOUNT;
+    }
+
+    private static Integer applyHolidayDiscount(Map<Menu, Integer> orderItems, Integer reserveDate) {
+        if (HolidayDiscount.isWithinPeriod(reserveDate)) {
+            return HolidayDiscount.calculate(orderItems);
+        }
+        return NO_DISCOUNT;
+    }
+
+    private static Integer applySpecialDiscount(Integer reserveDate) {
+        if (SpecialDiscount.isWithinPeriod(reserveDate)) {
+            return SpecialDiscount.AMOUNT;
+        }
+        return NO_DISCOUNT;
     }
 }
