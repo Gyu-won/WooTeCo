f14365a3cdc72183257b1c45f6cb0db8acbebc05
JUNG GYUWON <milestogo0905@naver.com>
2023-11-11 13:59:59 +0900

    test: 총 할인 금액의 합을 계산하는 테스트 추가

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -1,7 +1,9 @@
 package christmas.domain;
 
+import java.util.Map;
+
 public class Event {
-    public static int calculate() {
+    public static int apply(Map<Menu, Integer> orderItems, Integer reserveDate) {
         return 0;
     }
 }
--- src/test/java/christmas/domain/EventTest.java
+++ src/test/java/christmas/domain/EventTest.java
@@ -2,6 +2,8 @@ package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +11,37 @@ public class EventTest {
 
     @DisplayName("할인 금액의 합계를 계산")
     @Test
-    public void 총_할인_금액을_계산() {
+    public void 총_할인금액_합계가_0인경우() {
+        //given
+        Map<Menu, Integer> orderItems = new HashMap<>();
+        orderItems.put(Menu.Tapas, 1);
+        orderItems.put(Menu.ZeroCoke, 1);
+
+        Integer reserveDate = 26;
+
         //when
-        int discountAmount = Event.calculate();
+        int discountAmount = Event.apply(orderItems, reserveDate);
 
         //then
         assertThat(discountAmount).isEqualTo(0);
+    }
+
+    @DisplayName("할인 금액의 합계를 계산")
+    @Test
+    public void 총_할인금액이_0이_아닌_경우() {
+        //given
+        Map<Menu, Integer> orderItems = new HashMap<>();
+        orderItems.put(Menu.TBoneSteak, 1);
+        orderItems.put(Menu.BarbecueRibs, 1);
+        orderItems.put(Menu.ChocolateCake, 2);
+        orderItems.put(Menu.ZeroCoke, 1);
+
+        Integer reserveDate = 3;
+
+        //when
+        int discountAmount = Event.apply(orderItems, reserveDate);
+
+        //then
+        assertThat(discountAmount).isEqualTo(31246);
     }
 }
