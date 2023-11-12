152bf53d37dc9d67b669ad5d84d1b65492d47bce
JUNG GYUWON <milestogo0905@naver.com>
2023-11-13 01:37:21 +0900

    feat: 혜택금액이 5000원미만이면 아무 배지도 부여하지 않는 기능 추가

--- src/main/java/christmas/domain/service/event/Badge.java
+++ src/main/java/christmas/domain/service/event/Badge.java
@@ -1,7 +1,9 @@
 package christmas.domain.service.event;
 
 public class Badge {
+    private static final String NO_BADGE = "없음";
+
     public static String calculate(Integer totalEventReward) {
-        return null;
+        return NO_BADGE;
     }
 }
