07ede9753ca18ef72b8a460c26d804ee6d6b6ee4
JUNG GYUWON <milestogo0905@naver.com>
2023-11-13 01:42:39 +0900

    test: 혜택금엑이 5000원 이상, 10000원 미만인 경우 별 배지를 부여하는 테스트 추가

--- src/test/java/christmas/domain/service/event/BadgeTest.java
+++ src/test/java/christmas/domain/service/event/BadgeTest.java
@@ -4,12 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BadgeTest {
 
-    @DisplayName("혜택금액이 5000원 미만이면 아무 등급도 아니다.")
+    @DisplayName("혜택금액이 5000원 미만이면 배지가 없다.")
     @Test
-    public void 아무_등급이_아닌_경우() {
+    public void 아무_배지도_못받는_경우() {
         //given
         Integer totalEventReward = 4999;
 
@@ -18,5 +20,16 @@ public class BadgeTest {
 
         //then
         assertThat(rank).isEqualTo("없음");
+    }
+
+    @DisplayName("혜택금액이 5000원 이상 10000원 미만이면  없다.")
+    @ParameterizedTest
+    @ValueSource(ints = {5000, 9999})
+    public void 별_배지를_부여받는_경우(Integer totalEventReward) {
+        //when
+        String rank = Badge.calculate(totalEventReward);
+
+        //then
+        assertThat(rank).isEqualTo("별");
     }
 }
