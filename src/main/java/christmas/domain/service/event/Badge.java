ce5603ff8cdf19083bdfa58fdc8337fc523bc7c3
JUNG GYUWON <milestogo0905@naver.com>
2023-11-13 01:48:51 +0900

    feat: 혜택금액이 5000원 이상, 10000원 미만인 경우 별 배지를 부여하는 기능 추가

--- src/main/java/christmas/domain/service/event/Badge.java
+++ src/main/java/christmas/domain/service/event/Badge.java
@@ -2,8 +2,17 @@ package christmas.domain.service.event;
 
 public class Badge {
     private static final String NO_BADGE = "없음";
+    private static final String STAR = "별";
+    private static final Integer STAR_MINIMUM = 5000;
 
     public static String calculate(Integer totalEventReward) {
+        if (isStar(totalEventReward)) {
+            return STAR;
+        }
         return NO_BADGE;
+    }
+
+    private static boolean isStar(Integer totalEventReward) {
+        return totalEventReward >= STAR_MINIMUM;
     }
 }
