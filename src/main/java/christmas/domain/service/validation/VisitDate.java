0a13644042e7af01c2b9d3f781740c0e223c4195
JUNG GYUWON <milestogo0905@naver.com>
2023-11-12 12:52:27 +0900

    feat: 방문 날짜는 1이상 31 이하의 숫자만 가능하도록 기능 추가

--- src/main/java/christmas/domain/service/validation/VisitDate.java
+++ src/main/java/christmas/domain/service/validation/VisitDate.java
@@ -1,7 +1,35 @@
 package christmas.domain.service.validation;
 
+import christmas.view.Error;
+
 public class VisitDate {
+    private static final Integer MAXIMUM_RESERVATION_DEADLINE = 31;
+    private static final Integer MINIMUM_RESERVATION_DEADLINE = 1;
+
     public static Integer validate(String visitDate) {
-        return null;
+        return checkRange(toInteger(visitDate));
+    }
+
+    private static Integer checkRange(Integer parsedVisitDate) {
+        if (isSmallerThanMin(parsedVisitDate) || isBiggerThanMax(parsedVisitDate)) {
+            throw new IllegalArgumentException(Error.INVALID_VISIT_DATE.getMessage());
+        }
+        return parsedVisitDate;
+    }
+
+    private static boolean isBiggerThanMax(Integer parsedVisitDate) {
+        return parsedVisitDate > MAXIMUM_RESERVATION_DEADLINE;
+    }
+
+    private static boolean isSmallerThanMin(Integer parsedVisitDate) {
+        return parsedVisitDate < MINIMUM_RESERVATION_DEADLINE;
+    }
+
+    private static Integer toInteger(String visitDate) {
+        try {
+            return Integer.parseInt(visitDate);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(Error.INVALID_VISIT_DATE.getMessage());
+        }
     }
 }
