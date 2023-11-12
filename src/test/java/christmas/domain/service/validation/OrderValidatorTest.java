83e426cf032882217bfeedebb3fac7abcbafeb35
JUNG GYUWON <milestogo0905@naver.com>
2023-11-12 11:57:51 +0900

    test: 주문메뉴가 20개를 초과하는 경우 예외처리 테스트 추가

--- src/test/java/christmas/domain/service/validation/OrderValidatorTest.java
+++ src/test/java/christmas/domain/service/validation/OrderValidatorTest.java
@@ -3,6 +3,7 @@ package christmas.domain.service.validation;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,4 +18,15 @@ public class OrderValidatorTest {
                 .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 
+    @DisplayName("주문메뉴가 20개를 초과하는 경우 에러를 발생시킨다")
+    @Test
+    public void 주문메뉴가_20개를_초과한_경우() {
+        //given
+        String menusInput = "양송이수프-6,티본스테이크-6,초코케이크-3,제로콜라-6";
+
+        //when+then
+        assertThatThrownBy(() -> OrderValidator.validate(menusInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
 }
