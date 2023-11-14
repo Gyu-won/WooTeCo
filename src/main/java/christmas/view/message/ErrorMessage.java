259868ba86299a5b70feacde712baac62e5cd46b
JUNG GYUWON <milestogo0905@naver.com>
2023-11-14 23:21:15 +0900

    refactor: ErrorMessage 클래스의 상수 및 필드 변수에 static 및 final 추가

--- src/main/java/christmas/view/message/ErrorMessage.java
+++ src/main/java/christmas/view/message/ErrorMessage.java
@@ -4,8 +4,8 @@ public enum ErrorMessage {
     INVALID_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     INVALID_VISIT_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
-    private final String CODE = "[ERROR] ";
-    private String message;
+    private static final String CODE = "[ERROR] ";
+    private final String message;
 
     ErrorMessage(String message) {
         this.message = CODE + message;
