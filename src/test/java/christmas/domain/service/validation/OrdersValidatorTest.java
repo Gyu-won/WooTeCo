54136c2d681564ffc6489f34558686e109772ace
JUNG GYUWON <milestogo0905@naver.com>
2023-11-15 16:20:01 +0900

    test: 주문 메뉴의 입력 형식이 올바르지 않으면 에러를 발생시키는 테스트를 OrdersValidater 에 추가
    객체의 명확한 역할 구분을 위해 여러 주문들의 형식을 체크하는 기능을 OrdersValidator에서 수행하도록 테스트 코드를 변경

--- src/test/java/christmas/domain/service/validation/OrdersValidatorTest.java
+++ src/test/java/christmas/domain/service/validation/OrdersValidatorTest.java
@@ -10,6 +10,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class OrdersValidatorTest {
+
+    @DisplayName("주문 메뉴 입력의 형식이 올바르지 않으면 에러를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"해산물파스타-2,레드와인=1", "해산물파스타-2,"})
+    public void 주문메뉴_입력의_형식이_올바르지_않은_경우(String menusInput) {
+        //when+then
+        assertThatThrownBy(() -> OrdersValidator.validate(menusInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+    
     @DisplayName("음료만 주문 시 에러를 발생시킨다.")
     @ParameterizedTest
     @ValueSource(strings = {"제로콜라-3", "레드와인-1,샴폐인1"})
