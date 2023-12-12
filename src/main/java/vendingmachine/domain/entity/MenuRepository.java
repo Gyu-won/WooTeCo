6397758b58b76cf4796c4013e6a1b4e9ea05d993
JUNG GYUWON <milestogo0905@naver.com>
2023-12-12 14:34:09 +0900

    feat: 세미콜론으로 메뉴를 분리하는 기능 추가

--- src/main/java/vendingmachine/domain/entity/MenuRepository.java
+++ src/main/java/vendingmachine/domain/entity/MenuRepository.java
@@ -1,7 +1,19 @@
 package vendingmachine.domain.entity;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MenuRepository {
+    private static final Integer SPLIT_LIMIT = -1;
+    private static final String ITEM_DELIMITER = ",";
+    private static final String MENU_DELIMITER = ";";
+
     public static void create(String menusInput) {
-        
+        List<String> menus = splitMenu(menusInput);
+
+    }
+
+    private static List<String> splitMenu(String menusInput) {
+        return Arrays.asList(menusInput.split(MENU_DELIMITER, SPLIT_LIMIT));
     }
 }
