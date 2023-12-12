4c80775b84f93b7b58f5410826032b72df735497
JUNG GYUWON <milestogo0905@naver.com>
2023-12-12 22:08:09 +0900

    feat: 카테고리를 초기 세팅한다

--- src/main/java/menu/domain/entity/Category.java
+++ src/main/java/menu/domain/entity/Category.java
@@ -1,19 +1,25 @@
 package menu.domain.entity;
 
 public enum Category {
-    JAPANESE("일식"),
-    KOREAN("한식"),
-    CHINESE("중식"),
-    ASIAN("아시안"),
-    EUROPE("양식");
+    JAPANESE("일식", 1),
+    KOREAN("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    EUROPE("양식", 5);
 
     private final String name;
+    private final Integer index;
 
-    Category(String name) {
+    Category(String name, Integer index) {
         this.name = name;
+        this.index = index;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Integer getIndex() {
+        return index;
     }
 }
