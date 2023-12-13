84ebbe8eaf49d39fc35cccaf2997ff0babd5a224
JUNG GYUWON <milestogo0905@naver.com>
2023-12-13 21:40:20 +0900

    test: 재시작을 선택한 경우 true 값을 리턴하는 테스트 추가

--- src/test/java/bridge/domain/entity/RetryFlagTest.java
+++ src/test/java/bridge/domain/entity/RetryFlagTest.java
@@ -6,13 +6,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RetryFlagTest {
-    @DisplayName("재시작이 아닌경우")
+    @DisplayName("종료를 선택한경우 false 값을 리턴")
     @Test
-    public void 재시작이_아닌경우() {
+    public void 종료를_선택한_경우() {
         //when
         RetryFlag retryFlag = new RetryFlag("Q");
 
         //then
         assertThat(retryFlag.isRetry()).isFalse();
+    }
+
+    @DisplayName("재시작을 선택한 경우 true 값을 리턴")
+    @Test
+    public void 재시작을_선택한_경우() {
+        //when
+        RetryFlag retryFlag = new RetryFlag("R");
+
+        //then
+        assertThat(retryFlag.isRetry()).isTrue();
     }
 }
