15f2120dccf188752fdb8ba4d20c16ccc9ca00b4
JUNG GYUWON <milestogo0905@naver.com>
2023-12-13 15:09:15 +0900

    feat: 다리의 길이가 3보다 작으면 예외처리

--- src/main/java/bridge/domain/validation/BridgeSizeValidator.java
+++ src/main/java/bridge/domain/validation/BridgeSizeValidator.java
@@ -6,7 +6,14 @@ public class BridgeSizeValidator {
 
     public static int validateAndReturn(String bridgeSizeInput) {
         Integer parsedBridgeSize = toInteger(bridgeSizeInput);
+        if (isSmallerThanMinimumSize(parsedBridgeSize)) {
+            throw new IllegalArgumentException(Error.INVALID_BRIDGE_SIZE.getMessage());
+        }
         return parsedBridgeSize;
+    }
+
+    private static boolean isSmallerThanMinimumSize(Integer parsedBridgeSize) {
+        return parsedBridgeSize < 3;
     }
 
     private static Integer toInteger(String bridgeSizeInput) {
