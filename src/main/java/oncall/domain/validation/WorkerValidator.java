b52911c6a7b236550658216f7ed4bc32c5218c22
JUNG GYUWON <milestogo0905@naver.com>
2023-12-16 15:40:56 +0900

    feat: 사원의 숫자가 올바르지 않은 경우 예외처리

--- src/main/java/oncall/domain/validation/WorkerValidator.java
+++ src/main/java/oncall/domain/validation/WorkerValidator.java
@@ -2,6 +2,7 @@ package oncall.domain.validation;
 
 import java.util.Arrays;
 import java.util.List;
+import oncall.view.message.Error;
 
 public class WorkerValidator {
     private static final Integer SPLIT_LIMIT = -1;
@@ -9,8 +10,17 @@ public class WorkerValidator {
 
     public static List<String> validateAndReturn(String workerName) {
         List<String> workerNames = splitWorkerName(workerName);
+        checkValidSize(workerNames);
         return workerNames;
     }
+
+    private static void checkValidSize(List<String> workerNames) {
+        int workerNumbers = workerNames.size();
+        if (workerNumbers < 5 || workerNumbers > 35) {
+            throw new IllegalArgumentException(Error.INVALID_WORKER.getMessage());
+        }
+    }
+
 
     private static List<String> splitWorkerName(String workerName) {
         return Arrays.asList(workerName.split(DELIMITER, SPLIT_LIMIT));
--- src/main/java/oncall/view/message/Error.java
+++ src/main/java/oncall/view/message/Error.java
@@ -1,7 +1,8 @@
 package oncall.view.message;
 
 public enum Error {
-    INVALID_DATE("유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+    INVALID_DATE("유효하지 않은 입력 값입니다. 다시 입력해 주세요."),
+    INVALID_WORKER("유효하지 않은 사원 이름 입력 값입니다. 다시 입력해 주세요.");
 
     private static final String CODE = "[ERROR] ";
 
