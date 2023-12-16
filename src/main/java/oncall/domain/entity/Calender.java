70454791bbc54de3d5ec6cc99b9cc819119112dd
JUNG GYUWON <milestogo0905@naver.com>
2023-12-16 15:29:09 +0900

    feat: 해당 월의 휴일 리스트를 만드는 기능 추가

--- src/main/java/oncall/domain/entity/Calender.java
+++ src/main/java/oncall/domain/entity/Calender.java
@@ -1,15 +1,29 @@
 package oncall.domain.entity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Calender {
     private final Integer month;
     private final Map<Integer, String> weekdays = new HashMap<>();
+    private final List<Integer> holidays = new ArrayList<>();
 
     public Calender(Integer month, Weekday weekday) {
         this.month = month;
         create(month, weekday);
+        createHolidayList();
+    }
+
+    private void createHolidayList() {
+        Integer lastDay = Month.lastDay(month);
+        for (int day = 1; day <= lastDay; day++) {
+            String weekday = weekdays.get(day);
+            if (weekday.equals("토") || weekday.equals("일") || Month.isHoliday(month, day)) {
+                holidays.add(day);
+            }
+        }
     }
 
     private void create(Integer month, Weekday weekday) {
--- src/main/java/oncall/domain/entity/Month.java
+++ src/main/java/oncall/domain/entity/Month.java
@@ -1,9 +1,19 @@
 package oncall.domain.entity;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Month {
+    private static final Map<Integer, List<Integer>> holidays = Map.of(
+            1, List.of(1),
+            3, List.of(1),
+            5, List.of(5),
+            6, List.of(6),
+            8, List.of(15),
+            10, List.of(3, 9),
+            12, List.of(25)
+    );
     private static final Map<Integer, Integer> lastDay = new HashMap<>();
 
     static {
@@ -27,5 +37,13 @@ public class Month {
 
     public static Integer lastDay(Integer month) {
         return lastDay.get(month);
+    }
+
+    public static boolean isHoliday(Integer month, int day) {
+        if (holidays.containsKey(month)) {
+            List<Integer> holiday = holidays.get(month);
+            return holiday.contains(day);
+        }
+        return false;
     }
 }
